76403b8f004e725a7cd181155c398b984a89045d
ElyFura <dominik.seng@gmx.de>
2020-09-14 19:48:15 +0200

    0.1.0

---
+++ test/extension.js
@@ -0,0 +1,64 @@
+'use strict';
+var path = require('path');
+var assert = require('yeoman-assert');
+var helpers = require('yeoman-test');
+
+describe('nodecg:extension', function() {
+    context('running on new project', function() {
+        context('in file mode', function() {
+            before(function(done) {
+                helpers.run(path.join(__dirname, '../generators/extension'))
+                    .withPrompts({ type: 'file' })
+                    .on('end', done);
+            });
+
+            it('creates the file', function() {
+                assert.file('extension.js');
+            });
+        });
+
+        context('in folder mode', function() {
+            before(function(done) {
+                helpers.run(path.join(__dirname, '../generators/extension'))
+                    .withPrompts({ type: 'folder' })
+                    .on('end', done);
+            });
+
+            it('creates the file', function() {
+                assert.file('extension/index.js');
+            });
+        });
+    });
+
+    context('running on existing project', function() {
+        context('in file mode', function() {
+            before(function(done) {
+                helpers.run(path.join(__dirname, '../generators/extension'))
+                    .withPrompts({ type: 'file' })
+                    .on('ready', function(gen) {
+                        gen.fs.write(gen.destinationPath('extension.js'), 'foo');
+                    })
+                    .on('end', done);
+            });
+
+            it('does not overwrite previous extension file', function() {
+                assert.fileContent('extension.js', 'foo');
+            });
+        });
+
+        context('in folder mode', function() {
+            before(function(done) {
+                helpers.run(path.join(__dirname, '../generators/extension'))
+                    .withPrompts({ type: 'folder' })
+                    .on('ready', function(gen) {
+                        gen.fs.write(gen.destinationPath('extension/index.js'), 'foo');
+                    })
+                    .on('end', done);
+            });
+
+            it('does not overwrite previous extension file', function() {
+                assert.fileContent('extension/index.js', 'foo');
+            });
+        });
+    });
+});
